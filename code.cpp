5204c90ecfb55de14ffb723be6456cc424d7cc98
Mahak Singh <mahaksingh058@gmail.com>
2024-12-07 20:51:52 +0530

    added cpp file to repo

---
+++ code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "enter an integer: " ;
+    cin >>n;
+
+    if(n%2==0){
+        cout << "the entered integer is even";
+
+    }
+    else {
+        cout << "the entered integer is odd";
+    }
+
+    return 0;
+
+}
+
